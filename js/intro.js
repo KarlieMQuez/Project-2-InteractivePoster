aad469282e9dc206906be107e797d5ad1a13deb6
KarlieMQuez <157342147+KarlieMQuez@users.noreply.github.com>
2024-03-09 17:42:50 -0800

    Hidden icons and final note

--- js/intro.js
+++ js/intro.js
@@ -1,4 +1,21 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
+$(document).ready (function(){
 
 $("#healthy1").hide();
-$("#mental").hide();
+$("#healthy2").hide();
+$("#healthy3").hide();
+
+$("#sticky-note").hide();
+
+$("#pop1").hide();
+$("#pop2").hide();
+$("#pop3").hide();
+
+
+
+
+
+
+
+
+})
