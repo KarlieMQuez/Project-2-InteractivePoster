159801f56bfa59b0a03e31881fca9f921c93b662
KarlieMQuez <157342147+KarlieMQuez@users.noreply.github.com>
2024-03-11 12:52:07 -0700

    added jqueary for chrome

--- js/intro.js
+++ js/intro.js
@@ -11,13 +11,21 @@ $("#pop1").hide();
 $("#pop2").hide();
 $("#pop3").hide();
 
+$("#chrome-open").hide();
+
 $("#open-client").hide();
 
 $("#chrome").clicked(function (){
-$("#pop1").show();
+$("#chrome-open").show();
+});
+
+$("#exit-email").clicked(function(){
+    $("#pop1").show();
+    $("#chrome-open").slideDown(1000);
+
+});
 
 
-})
 // When Chrome is clicked Email open, email sound plays, email sounds repeat, pop1 appears, phone rings
 
 // When Zoom is clicked, video appears, pop2 appears, pop3 appears
