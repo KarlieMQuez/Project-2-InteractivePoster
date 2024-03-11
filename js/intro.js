1d468c564e63fdcc2e356d8ba346645e8959a8f6
KarlieMQuez <157342147+KarlieMQuez@users.noreply.github.com>
2024-03-10 23:37:14 -0700

    Class title changed and details added in the open client folder

--- js/intro.js
+++ js/intro.js
@@ -11,6 +11,11 @@ $("#pop1").hide();
 $("#pop2").hide();
 $("#pop3").hide();
 
+// When Chrome is clicked Email open, email sound plays, email sounds repeat, pop1 appears, phone rings
+
+// When Zoom is clicked, video appears, pop2 appears, pop3 appears
+
+// when Zoom X is closed, Notifications of files repeats, phone rings more
 
 
 
