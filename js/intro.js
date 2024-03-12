347934532ca5921a3c0fd1c566d2ee562253343a
KarlieMQuez <157342147+KarlieMQuez@users.noreply.github.com>
2024-03-11 20:24:16 -0700

    fixed audio

--- js/intro.js
+++ js/intro.js
@@ -1,8 +1,8 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 $(document).ready (function(){
 
-var ping=
-
+var ding = document.getElementById("email-ding");
+var chat = document.getElementById("chat-ring");
 
 $("#healthy1").hide();
 $("#healthy2").hide();
@@ -19,10 +19,12 @@ $("#boss-email").hide();
 
 $("#open-client").hide();
 
+$("#teams-open").hide();
+
 
 $("#chrome").click( function (){
     $("#chrome-open").show();
-    $("#ding").play();
+
 });
 
 $("#open-me").click (function (){
@@ -41,7 +43,18 @@ $("#client").click( function (){
 
 $("#close-client").click( function(){
     $("#open-client").hide();
+    $("#pop2").show();
+    chat.play();
 
+});
+
+$("#teams").click(function(){
+    $("#teams-open").show();
+});
+
+$("#teams-close").click(function(){
+    $("#teams-open").hide();
+    ding.play();
 });
 
 
